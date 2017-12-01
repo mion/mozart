170fa0b8985ff9139eb5813ff24ab732b9af7a77
Gabriel Vieira <gluisvieira@gmail.com>
2017-12-01 00:01:03 -0200

    Add notes

--- game.js
+++ game.js
@@ -1,142 +1,146 @@
-/*
-
-    http://brm.io/matter-js/docs/classes/Body.html
-
- */
-
-
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies,
-    Body = Matter.Body,
-    Vector = Matter.Vector,
-    log = console.log;
-
-// create an engine
-var engine = Engine.create();
-
-// create a renderer
-var render = Render.create({
-    element: document.body,
-    engine: engine
-});
-
-// create two boxes and a ground
-var bodies = {
-    hero: Bodies.rectangle(300, 200, 50, 50),
-    boxA: Bodies.rectangle(400, 200, 80, 80),
-    boxB: Bodies.rectangle(450, 50, 80, 80),
-    boxC: Bodies.rectangle(100, 50, 80, 80),
-    ground: Bodies.rectangle(400, 610, 810, 60, { isStatic: true }),
-    ground2: Bodies.rectangle(425, 510, 405, 30, { isStatic: true }),
+var noteByFrequency = {
+  "C0":	16.35,
+  "C#0/Db0": 17.32,
+  "D0":	18.35,
+  "D#0/Eb0": 19.45,
+  "E0":	20.60,
+  "F0":	21.83,
+  "F#0/Gb0": 23.12,
+  "G0":	24.50,
+  "G#0/Ab0": 25.96,
+  "A0":	27.50,
+  "A#0/Bb0": 29.14,
+  "B0":	30.87,
+  "C1":	32.70,
+  "C#1/Db1": 34.65,
+  "D1":	36.71,
+  "D#1/Eb1": 38.89,
+  "E1":	41.20,
+  "F1":	43.65,
+  "F#1/Gb1": 46.25,
+  "G1":	49.00,
+  "G#1/Ab1": 51.91,
+  "A1":	55.00,
+  "A#1/Bb1": 58.27,
+  "B1":	61.74,
+  "C2":	65.41,
+  "C#2/Db2": 69.30,
+  "D2":	73.42,
+  "D#2/Eb2": 77.78,
+  "E2":	82.41,
+  "F2":	87.31,
+  "F#2/Gb2": 92.50,
+  "G2":	98.00,
+  "G#2/Ab2": 103.83,
+  "A2":	110.00,
+  "A#2/Bb2": 116.54,
+  "B2":	123.47,
+  "C3":	130.81,
+  "C#3/Db3": 138.59,
+  "D3":	146.83,
+  "D#3/Eb3": 155.56,
+  "E3":	164.81,
+  "F3":	174.61,
+  "F#3/Gb3": 185.00,
+  "G3":	196.00,
+  "G#3/Ab3": 207.65,
+  "A3":	220.00,
+  "A#3/Bb3": 233.08,
+  "B3":	246.94,
+  "C4":	261.63,
+  "C#4/Db4": 277.18,
+  "D4":	293.66,
+  "D#4/Eb4": 311.13,
+  "E4":	329.63,
+  "F4":	349.23,
+  "F#4/Gb4": 369.99,
+  "G4":	392.00,
+  "G#4/Ab4": 415.30,
+  "A4":	440.00,
+  "A#4/Bb4": 466.16,
+  "B4":	493.88,
+  "C5":	523.25,
+  "C#5/Db5": 554.37,
+  "D5":	587.33,
+  "D#5/Eb5": 622.25,
+  "E5":	659.25,
+  "F5":	698.46,
+  "F#5/Gb5": 739.99,
+  "G5":	783.99,
+  "G#5/Ab5": 830.61,
+  "A5":	880.00,
+  "A#5/Bb5": 932.33,
+  "B5":	987.77,
+  "C6":	1046.50,
+  "C#6/Db6": 1108.73,
+  "D6":	1174.66,
+  "D#6/Eb6": 1244.51,
+  "E6":	1318.51,
+  "F6":	1396.91,
+  "F#6/Gb6": 1479.98,
+  "G6":	1567.98,
+  "G#6/Ab6": 1661.22,
+  "A6":	1760.00,
+  "A#6/Bb6": 1864.66,
+  "B6":	1975.53,
+  "C7":	2093.00,
+  "C#7/Db7": 2217.46,
+  "D7":	2349.32,
+  "D#7/Eb7": 2489.02,
+  "E7":	2637.02,
+  "F7":	2793.83,
+  "F#7/Gb7": 2959.96,
+  "G7":	3135.96,
+  "G#7/Ab7": 3322.44,
+  "A7":	3520.00,
+  "A#7/Bb7": 3729.31,
+  "B7":	3951.07,
+  "C8":	4186.01,
+  "C#8/Db8": 4434.92,
+  "D8":	4698.63,
+  "D#8/Eb8": 4978.03,
+  "E8":	5274.04,
+  "F8":	5587.65,
+  "F#8/Gb8": 5919.91,
+  "G8":	6271.93,
+  "G#8/Ab8": 6644.88,
+  "A8":	7040.00,
+  "A#8/Bb8": 7458.62,
+  "B8":	7902.13,
 };
 
-// add all of the bodies to the world
-World.add(engine.world, _.map(_.values(bodies)));
-
-// run the engine
-Engine.run(engine);
-
-// run the renderer
-Render.run(render);
-//
-//
-////////////////////////////////////////////////////////////////////////////////
-//
-// Commands
-//
-////////////////////////////////////////////////////////////////////////////////
-var commands = {};
-
-commands.box = function command_box(x, y, width, height) {
-    var b = Bodies.rectangle(x, y, width, height);
-    World.add(engine.world, [b]);
-    Body.applyForce(b, b.position, Vector.create(0.1, 0.1));
+var music = {
+  amplitude: 0.0
 };
-
-window['qwe'] = function qwe(prop) {
-    return commands[prop];
-};
-
-const _horizontalForce = window['hf'] = 0.01;
-const _verticalForce = window['vf'] = 0.09;
-var isMovingLeft = false;
-var isMovingRight = false;
-
-document.addEventListener('keydown', (event) => {
-    const keyName = event.key;
-    log('key down: ' + keyName);
-    if (keyName === 'j') {
-        isMovingLeft = true;
-    }
-    if (keyName === 'l') {
-        isMovingRight = true;
-    }
-});
-
-let jump = (force) => {
-    Body.applyForce(bodies.hero, bodies.hero.position, Vector.create(0, -1 * force));
-};
-
-document.addEventListener('keyup', (event) => {
-    var verticalForce = _verticalForce / (1 + bodies.hero.angularSpeed);
-    const keyName = event.key;
-    log('key up: ' + keyName);
-    if (keyName === 'k') {
-        jump(verticalForce);
-    }
-    if (keyName === 'j') {
-        isMovingLeft = false;
-    }
-    if (keyName === 'l') {
-        isMovingRight = false;
-    }
-});
-
-const DELAY_MS = 100;
-var interval = window.setInterval(() => {
-    var horizontalForce = _horizontalForce / (1 + bodies.hero.angularSpeed);
-    if (isMovingLeft) {
-        let origin = Vector.create(
-            bodies.hero.position.x - bodies.hero.width,
-            bodies.hero.position.y);
-        Body.applyForce(bodies.hero, bodies.hero.position, Vector.create(-1 * horizontalForce, 0));
-    }
-    if (isMovingRight) {
-        Body.applyForce(bodies.hero, bodies.hero.position, Vector.create(+1 * horizontalForce, 0));
-    }
-}, DELAY_MS);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Gamepad
 ////////////////////////////////////////////////////////////////////////////////
-
 const gamepad = new Gamepad();
 
 gamepad.on('connect', e => {
-    console.log(`controller ${e.index} connected!`);
+  console.log(`controller ${e.index} connected!`);
 });
 
 gamepad.on('disconnect', e => {
-    console.log(`controller ${e.index} disconnected!`);
+  console.log(`controller ${e.index} disconnected!`);
 });
 
 gamepad.on('press', 'button_1', () => {
-    console.log('button 1 was pressed!');
+  console.log('button 1 was pressed!');
 });
 
 gamepad.on('hold', 'button_1', () => {
-    console.log('button 1 is being held!');
+  console.log('button 1 is being held!');
 });
 
 gamepad.on('release', 'button_1', () => {
-    console.log('button 1 was released!');
+  console.log('button 1 was released!');
 });
 
 gamepad.on('hold', 'shoulder_bottom_right', e => {
-    console.log(`shoulder_bottom_right has a value of ${e.value}!`);
+  console.log(`shoulder_bottom_right has a value of ${e.value}!`);
+  music.amplitude = e.value;
 });
 
 //
@@ -150,7 +154,7 @@ function setup() {
   createCanvas(720, 256);
 
   osc = new p5.TriOsc(); // set frequency and type
-  osc.amp(.5);
+  osc.amp(music.amplitude);
 
   fft = new p5.FFT();
   osc.start();
@@ -171,46 +175,12 @@ function draw() {
 
   // change oscillator frequency based on mouseX
   var freq = map(mouseX, 0, width, 40, 880);
+  console.log(freq);
   osc.freq(freq);
 
-  var amp = map(mouseY, 0, height, 1, .01);
-  osc.amp(amp);
+  // var amp = map(mouseY, 0, height, 1, .01);
+  osc.amp(music.amplitude);
+
+  text("Amplitude: " + music.amplitude, 10, 10, 200, 100); // Text wraps within text box
+  text("Frequency: " + freq, 10, 50, 200, 100); // Text wraps within text box
 }
-
-/*
-function setup() {
-  createCanvas(1000, 1000);
-}
-
-function draw() {
-  // desenhando o retangulo
-  var rectangleX = 30;
-  var rectangleY = 75;
-  var rectangleWidth = 250;
-  var rectangleHeight = 200;
-
-  rect(rectangleX, rectangleY, rectangleWidth, rectangleHeight);
-
-  // desenhando o triangulo
-  var x1 = 30;
-  var y1 = 75;
-
-  var x2 = 58;
-  var y2 = 20;
-
-  var x3 = 86;
-  var y3 = 75;
-
-  triangle(x1, y1, x2, y2, x3, y3);
-}
-*/
-
-/*
-function create() {
-    var block = new Block({mass: 10, width: 100, height: 50});
-    var node = new Node({mass: 10, radius: 5, origin: [0,0]});
-    var edge = new Edge(block, node, {
-        length: 10
-    });
-}
-*/
